c7c1805861cd248023c9c33123d4a6e36e90ce39
bindu272 <bindupatil272@gmail.com>
2024-06-29 22:14:53 +0530

    added footer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo from "./Logo";
-
+import { FaSearch } from "react-icons/fa";
+import { FaRegCircleUser } from "react-icons/fa6";
 const Header = () => {
   return (
     <header className="h-16 shadow-md">
@@ -8,11 +9,22 @@ const Header = () => {
         <div className="">
           <Logo w={90} h={50} />
         </div>
-        <div>
-          search
+        <div className="flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2">
+          <input
+            type="text"
+            placeholder="search product here.."
+            className="w-full outline-none"
+          />
+          <div className="text-lg min-w-[50px] bg-red-600 h-8 flex items-center justify-center rounded-r-full text-white">
+            <FaSearch />
+          </div>
         </div>
-        <div>
-          user icon and cards
+        <div className="flex items-center gap-7">
+          <div className="relative flex justify-center">
+            <div className="text-3xl cursor-pointer relative flex justify-center">
+              <FaRegCircleUser />
+            </div>
+          </div>
         </div>
       </div>
     </header>
